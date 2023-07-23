b3b589b8c8f8e8cde02404754e36f33dc8adc082
HJ Kim <pknojlh27@naver.com>
2023-07-23 15:16:41 +0900

    feat : Reducer decrement 추가

--- src/store/counter.js
+++ src/store/counter.js
@@ -8,10 +8,15 @@
 
 //action type
 const INCREMENT = 'counter/increment'; //중복이므로 유지보수&오타를 위해 상수화
+const DECREMENT = 'counter/decrement';
 
 //action creator
 export function increaseCounter() {
   return { type: INCREMENT };
+}
+
+export function decreaseCounter() {
+  return { type: DECREMENT };
 }
 
 //reducer
@@ -22,6 +27,10 @@ const INITIAL_STATE = 0;
 export function counterReducer(state = INITIAL_STATE, action) {
   if (action.type === INCREMENT) {
     return state + 1;
+  }
+
+  if (action.type === DECREMENT) {
+    return state - 1;
   }
 
   return state;
